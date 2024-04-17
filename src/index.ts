58b0f4206708fb35b85852f887f0aee974605476
Cole Blender <coleblender@gmail.com>
2024-04-17 11:25:42 -0700

    add cli code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-export const hello = () => {
-  console.log("hello");
-};
+#!/usr/bin/env node
+import { add } from "@/src/commands/add";
+import { diff } from "@/src/commands/diff";
+import { init } from "@/src/commands/init";
+import { Command } from "commander";
 
-export const world = () => {
-  console.log("world");
-};
+import { getPackageInfo } from "./utils/get-package-info";
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+async function main() {
+  const packageInfo = await getPackageInfo();
+
+  const program = new Command()
+    .name("yazzi-ui")
+    .description("add components and dependencies to your project")
+    .version(
+      packageInfo.version || "1.0.0",
+      "-v, --version",
+      "display the version number"
+    );
+
+  program.addCommand(init).addCommand(add).addCommand(diff);
+
+  program.parse();
+}
+
+main();
